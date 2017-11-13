ad122f8a00bc05359af284055c3dd8257f73b81f
petr-tik <petr-tik@users.noreply.github.com>
2017-11-13 20:00:08 +0000

    csv reads the whole file

--- src/clj_pubs/core.clj
+++ src/clj_pubs/core.clj
@@ -4,9 +4,10 @@
 (require '[clojure.data.csv :as csv]
          '[clojure.java.io :as io])
 
-
+(def pubs (with-open [reader (io/reader "open_pubs.csv")]
+           (csv/read-csv reader)))
 
 (defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+             "I don't do a whole lot ... yet."
+             [& args]
+             (println "hello"))
