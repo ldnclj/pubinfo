d19b514bd28f9e32713d36ce5d47036207884d44
petr-tik <petr-tik@users.noreply.github.com>
2017-11-13 19:40:56 +0000

    lein init

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to clj-pubs
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/clj_pubs/core_test.clj
@@ -0,0 +1,7 @@
+(ns clj-pubs.core-test
+  (:require [clojure.test :refer :all]
+            [clj-pubs.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
