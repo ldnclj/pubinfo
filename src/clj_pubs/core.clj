53d1d7060182f657f92402ee417ccfee6a77a2e0
Xing Yang <xor@jecity.net>
2017-11-13 20:04:34 +0000

    Realize pubs

--- src/clj_pubs/core.clj
+++ src/clj_pubs/core.clj
@@ -5,7 +5,7 @@
          '[clojure.java.io :as io])
 
 (def pubs (with-open [reader (io/reader "open_pubs.csv")]
-           (csv/read-csv reader)))
+            (doall (csv/read-csv reader))))
 
 (defn -main
              "I don't do a whole lot ... yet."
